e65d782626f83e94e97f16957b4cd76c38b7248e
FahreziP <fahrezipurwadianitra@gmail.com>
2025-05-08 10:49:39 +0700

    menambahkan fungsi traverse untuk menampilkan data

--- singlelinklist.cpp
+++ singlelinklist.cpp
@@ -80,5 +80,18 @@ class SingleLinkList{
             return true;
         }
 
-        
+        void traverse(){
+            if (listEmpty()){
+                cout << "\nList Kosong\n";
+            }
+            else{
+                cout << "\nData didalam list adalah:\n";
+                Node *currentNode = START;
+                while (currentNode != NULL){
+                    cout << currentNode->noMhs << endl;
+                    currentNode = currentNode->next;
+                }
+                cout << endl;
+            }
+        }
 };
