cdbbe1a4d3298f9a53dbab318ac3f8e195ebbce6
FahreziP <fahrezipurwadianitra@gmail.com>
2025-05-08 10:42:31 +0700

    menambahkan fungsi bool delnode

--- singlelinklist.cpp
+++ singlelinklist.cpp
@@ -65,4 +65,20 @@ class SingleLinkList{
 
             return (*current != NULL);
         }
+
+        bool delNode(int nim){
+            Node *current, *previous;
+            if (!Search(nim, &previous, &current))
+                return false;
+
+            if (current == START)
+                START = START->next;
+            else
+                previous->next = current->next;
+
+            delete current;
+            return true;
+        }
+
+        
 };
