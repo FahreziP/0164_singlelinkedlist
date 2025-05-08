a26c26910ab53c27a51e80a5d122675fd758f193
FahreziP <fahrezipurwadianitra@gmail.com>
2025-05-08 11:24:19 +0700

    menambahkan case 4

--- singlelinklist.cpp
+++ singlelinklist.cpp
@@ -138,6 +138,28 @@ int main(){
             case '3':{
                 mhs.traverse();
             }
+            break;
+            case '4':{
+                if (mhs.listEmpty() == true){
+                    cout << "\nList Kosong\n";
+                    break;
+                }
+                Node *previous, *current;
+                cout << endl
+                     << "Masukkan no Mahasiswa yang dicari : ";
+                cin >> nim;
+                if (mhs.Search(nim, &previous, &current) == false)
+                    cout << endl
+                         << "Data tidak ditemukan" << endl;
+                else{
+                    cout << endl
+                         << "Data ditemukan" << endl;
+                    cout << "\nNo Mahasiswa: " << current->noMhs << endl;
+                    cout << "\n";
+                }
+            }
+            break;
+            
         }
     }
 }
