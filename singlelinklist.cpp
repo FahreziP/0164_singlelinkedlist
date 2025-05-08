89bfe77382b6650d76e02338e5a3897e2d675f49
FahreziP <fahrezipurwadianitra@gmail.com>
2025-05-08 10:16:41 +0700

    membuat perulangan while jika nim terdapat duplikasi

--- singlelinklist.cpp
+++ singlelinklist.cpp
@@ -34,5 +34,16 @@ class List{
                 return;
             }
             
+            Node *previous = START;
+            Node *current = START;
+
+            while((current != NULL) && (nim >= current->noMhs)){
+                if (nim == current->noMhs){
+                    cout << "\nDuplikasi no Mhs tidak diizinkan\n";
+                    return;
+                }
+                previous = current;
+                current = current->next;
+            }
         }
 };
